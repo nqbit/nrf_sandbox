b8616115ef7b1ec96a8378b8fb5e9d24dee52c72
nqbit <nathaniel.quillin@gmail.com>
2015-03-01 23:51:13 -0800

    moved ldflags to compile.mk for project specific map files.

--- build/compile.mk
+++ build/compile.mk
@@ -1,22 +1,57 @@
 PROJECT_BUILD_DIR := $(OUTPUT_DIR)/$(PROJECT)
 PROJECT_SRC_DIR := $(PROJECT_DIR)/$(PROJECT)
+PROJECT_OUT_BIN := $(PROJECT_BUILD_DIR)/$(PROJECT).bin
 
-$(PROJECT): $(PROJECT_BUILD_DIR) $(PROJECT).bin
+$(PROJECT): $(PROJECT_BUILD_DIR) $(PROJECT_OUT_BIN)
 	echo done
 
 $(PROJECT_BUILD_DIR):
 	mkdir -p $(PROJECT_BUILD_DIR)
 
+# Add in the files required by the project
 PROJECT_SRC_C := $(addprefix $(PROJECT_SRC_DIR)/, $(filter %.c, $(PROJECT_SRC)))
-PROJECT_SRC_S := $(filter %.s, $(PROJECT_SRC))
-PROJECT_OBJECTS_C := $(PROJECT_BUILD_DIR)/$(patsubst %.c,%.o, $(filter %.c, $(PROJECT_SRC)))
-PROJECT_OBJECTS_S := $(PROJECT_BUILD_DIR)/$(patsubst %.s,%.o, $(PROJECT_SRC_S))
+PROJECT_SRC_S := $(addprefix $(PROJECT_SRC_DIR)/, $(filter %.s, $(PROJECT_SRC)))
+PROJECT_OBJECTS_C := $(addprefix $(PROJECT_BUILD_DIR)/,$(patsubst %.c,%.o, $(filter %.c, $(PROJECT_SRC))))
+PROJECT_OBJECTS_S := $(addprefix $(PROJECT_BUILD_DIR)/,$(patsubst %.s,%.o, $(filter %.s, $(PROJECT_SRC))))
+
+# Add in the files required by nordic
+NORDIC_SRC_S := $(filter %.s, $(NORDIC_SRC))
+NORDIC_SRC_C := $(filter %.c, $(NORDIC_SRC))
+NORDIC_OBJECTS_C := $(addprefix $(PROJECT_BUILD_DIR)/,$(patsubst %.c,%.o, $(filter %.c, $(notdir $(NORDIC_SRC)))))
+NORDIC_OBJECTS_S := $(addprefix $(PROJECT_BUILD_DIR)/,$(patsubst %.s,%.o, $(filter %.s, $(notdir $(NORDIC_SRC)))))
+
+OBJECTS := $(PROJECT_OBJECTS_C) $(PROJECT_OBJECTS_S) $(NORDIC_OBJECTS_C) $(NORDIC_OBJECTS_S)
+
+# Include files
+INCLUDE_DIR := $(addprefix -I,$(NORDIC_INC))
+LDFLAGS := \
+	-Xlinker \
+	-Map=$(PROJECT_BUILD_DIR)/$(PROJECT).map \
+	-mthumb \
+	-mabi=aapcs \
+	-Lsdk/components/toolchain/gcc \
+	-T$(LINKER_SCRIPT) \
+	-mcpu=cortex-m0 \
+	-Wl,--gc-sections \
+	--specs=nano.specs \
+	-lc \
+	-lnosys
 
 $(PROJECT_OBJECTS_C): $(PROJECT_SRC_C)
-	$(CC) $(GLOBAL_CFLAGS) $(CFLAGS) $(GLOBAL_DEFINES) $(NO_LINKER) $(INCLUDE_DIR) -o $@ $<
+	$(CC) $(CFLAGS) $(DEFINES) $(NO_LINKER) $(INCLUDE_DIR) -o $@ $<
 
-$(PROJECT_OBJECTS_S): $(PROJECT_SRC_S)
-	echo NEEDS TO BE COMPLETED
+$(NORDIC_OBJECTS_C): $(NORDIC_SRC_C)
+	$(CC) $(CFLAGS) $(DEFINES) $(NO_LINKER) $(INCLUDE_DIR) -o $@ $<
 
-$(PROJECT).bin: $(PROJECT_OBJECTS_C)
-	echo $(PROJECT_OBJECTS_C)
+$(NORDIC_OBJECTS_S): $(NORDIC_SRC_S)
+	$(CC) $(ASMFLAGS) $(DEFINES) $(NO_LINKER) $(INCLUDE_DIR) -o $@ $<
+
+# $(PROJECT_OBJECTS_S): $(PROJECT_SRC_S)
+# 	$(CC) $(GLOBAL_SFLAGS) $(GLOBAL_DEFINES) $(NO_LINKER) $(INCLUDE_DIR) -o $@ $<
+
+OBJECTS: $(PROJECT_OBJECTS_C) $(PROJECT_OBJECTS_S) $(NORDIC_OBJECTS_C) $(NORDIC_OBJECTS_S)
+
+$(PROJECT_OUT_BIN): OBJECTS
+	@echo Linking target: $@
+	$(CC) $(LDFLAGS) $(OBJECTS) $(LIBS) -o $@
+
--- build/init.mk
+++ build/init.mk
@@ -2,13 +2,14 @@ BUILD_DIR := build
 PROJECT_DIR := projects
 OUTPUT_DIR := out
 
-
 TOOLCHAIN_PREFIX ?= /home/quasar/dev/gcc-arm-none-eabi-4_8-2014q1/bin/arm-none-eabi-
 CC ?= $(TOOLCHAIN_PREFIX)gcc
+LD ?= $(TOOLCHAIN_PREFIX)ld
 
 NO_LINKER := -c
+LINKER_SCRIPT := sdk/components/toolchain/gcc/gcc_nrf51_blank_xxaa.ld
 
-GLOBAL_CFLAGS := \
+CFLAGS := \
 	-mcpu=cortex-m0 \
 	-mthumb \
 	-mabi=aapcs \
@@ -24,11 +25,36 @@ GLOBAL_CFLAGS := \
 	-flto \
 	-fno-builtin
 
-GLOBAL_DEFINES := \
+DEFINES := \
 	-DNRF51 \
-	-DBLE_STACK_SUPPORT_REQD \
+	-DBSP_DEFINES_ONLY \
 	-DS110 -DSOFTDEVICE_PRESENT \
 	-DBOARD_PCA10000
+
+ASMFLAGS := \
+	-x assembler-with-cpp
+
+LDFLAGS := \
+	-Xlinker \
+	-mthumb \
+	-mabi=aapcs \
+	-Lsdk/components/toolchain/gcc \
+	-T$(LINKER_SCRIPT) \
+	-mcpu=cortex-m0 \
+	-Wl,--gc-sections \
+	--specs=nano.specs \
+	-lc \
+	-lnosys
+
+# Nordic Source Files
+NORDIC_SRC := sdk/components/toolchain/gcc/gcc_startup_nrf51.s
+NORDIC_SRC += sdk/components/toolchain/system_nrf51.c
+NORDIC_SRC += sdk/components/drivers_nrf/hal/nrf_delay.c
+
+# Nordic Include Files
+NORDIC_INC := sdk/components/drivers_nrf/hal
+NORDIC_INC += sdk/components/toolchain/gcc
+NORDIC_INC += sdk/components/toolchain
 
 # Build
 BUILD_BIN := $(BUILD_DIR)/compile.mk
